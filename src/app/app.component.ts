c172025a10b5eabf4ec5f608136eb920d6968a74
leightonlam <leighton.lam@mudbath.com.au>
2024-06-18 11:41:05 +1000

    connect heroes data from component to template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,24 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SuperHeroService } from './services/super-hero.service';
+import { SuperHero } from './models/super-hero';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgFor],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'SuperHero.UI';
+  heroes: SuperHero[] = [];
+
+  constructor(private superHeroService: SuperHeroService) {}
+
+  ngOnInit(): void {
+    this.heroes = this.superHeroService.getSuperHeroes();
+    console.log(this.heroes);
+  }
 }
