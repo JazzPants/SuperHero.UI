9057d6c38b22ddb15da76ff90dc937a8c24dfb5f
leightonlam <leighton.lam@mudbath.com.au>
2024-06-20 17:08:39 +1000

    add remaining service methods, supply methods to edit component, add eventEmitter for edit component (re-render hero list) and add/edit hero functions, add edit component to app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,17 +3,19 @@ import { RouterOutlet } from '@angular/router';
 import { SuperHeroService } from './services/super-hero.service';
 import { SuperHero } from './models/super-hero';
 import { NgFor } from '@angular/common';
+import { EditHeroComponent } from './components/edit-hero/edit-hero.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NgFor],
+  imports: [RouterOutlet, NgFor, EditHeroComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'SuperHero.UI';
   heroes: SuperHero[] = [];
+  heroToEdit?: SuperHero;
 
   constructor(private superHeroService: SuperHeroService) {}
 
@@ -23,5 +25,17 @@ export class AppComponent {
     this.superHeroService
       .getSuperHeroes()
       .subscribe((result: SuperHero[]) => (this.heroes = result));
+  }
+
+  updateHeroList(heroes: SuperHero[]) {
+    this.heroes = heroes;
+  }
+
+  initNewHero() {
+    this.heroToEdit = new SuperHero();
+  }
+
+  editHero(hero: SuperHero) {
+    this.heroToEdit = hero;
   }
 }
--- src/app/components/edit-hero/edit-hero.component.ts
+++ src/app/components/edit-hero/edit-hero.component.ts
@@ -1,7 +1,9 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { SuperHero } from '../../models/super-hero';
 import { FormsModule } from '@angular/forms';
 import { NgIf } from '@angular/common';
+// import { EventEmitter } from 'stream';
+import { SuperHeroService } from '../../services/super-hero.service';
 
 @Component({
   selector: 'app-edit-hero',
@@ -14,10 +16,33 @@ import { NgIf } from '@angular/common';
 // implements OnInit
 export class EditHeroComponent {
   @Input() hero?: SuperHero;
+  @Output() heroesUpdated = new EventEmitter<SuperHero[]>();
 
-  // constructor() {}
+  constructor(private superHeroService: SuperHeroService) {}
 
-  // ngOnInit(): void {
+  ngOnInit(): void {
+    console.log('first render', this.hero);
+  }
 
-  // }
+  ngOnChanges(): void {
+    console.log('selecting a hero to create/edit', this.hero);
+  }
+
+  updateHero(hero: SuperHero) {
+    console.log('update hero', hero);
+    this.superHeroService
+      .updateHero(hero)
+      .subscribe((heroes: SuperHero[]) => this.heroesUpdated.emit(heroes));
+  }
+  deleteHero(hero: SuperHero) {
+    this.superHeroService
+      .deleteHero(hero)
+      .subscribe((heroes: SuperHero[]) => this.heroesUpdated.emit(heroes));
+  }
+
+  createHero(hero: SuperHero) {
+    this.superHeroService
+      .createHero(hero)
+      .subscribe((heroes: SuperHero[]) => this.heroesUpdated.emit(heroes));
+  }
 }
