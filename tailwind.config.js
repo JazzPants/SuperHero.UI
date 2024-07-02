ac1ac6adb0effbb85898b838a03ee34a22cdff24
leightonlam <leighton.lam@mudbath.com.au>
2024-07-02 10:59:31 +1000

    start building new theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./src/**/*.{html,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        darkblue: "#00008b",
+        darkred: "#8b0000",
+      },
+    },
   },
   plugins: [],
 };
