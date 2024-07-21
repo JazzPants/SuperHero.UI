36870bbb624936cc5d7dad931124d2effcfb8991
JazzPants14 <leighton.l14@gmail.com>
2024-07-21 17:10:26 +1000

    convert to reactive form, send formvalue to createhero function, patchvalue for edit form

--- src/app/components/edit-hero/edit-hero.component.ts
+++ src/app/components/edit-hero/edit-hero.component.ts
@@ -1,6 +1,13 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  Input,
+  OnInit,
+  Output,
+  SimpleChanges,
+} from '@angular/core';
 import { SuperHero } from '../../models/super-hero';
-import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule, FormControl, FormGroup } from '@angular/forms';
 import { NgIf } from '@angular/common';
 // import { EventEmitter } from 'stream';
 import { SuperHeroService } from '../../services/super-hero.service';
@@ -8,13 +15,22 @@ import { SuperHeroService } from '../../services/super-hero.service';
 @Component({
   selector: 'app-edit-hero',
   standalone: true,
-  imports: [NgIf, FormsModule],
+  imports: [NgIf, ReactiveFormsModule],
   templateUrl: './edit-hero.component.html',
   styleUrl: './edit-hero.component.css',
 })
 
 // implements OnInit
 export class EditHeroComponent {
+  heroForm = new FormGroup({
+    name: new FormControl('A super hero name'),
+    firstName: new FormControl('123'),
+    lastName: new FormControl(''),
+    superPower: new FormControl(''),
+    location: new FormControl(''),
+    items: new FormControl([]),
+  });
+
   @Input() hero?: SuperHero;
   @Output() heroesUpdated = new EventEmitter<SuperHero[]>();
 
@@ -24,7 +40,23 @@ export class EditHeroComponent {
     console.log('first render', this.hero);
   }
 
-  ngOnChanges(): void {
+  ngOnChanges(changes: SimpleChanges): void {
+    // if (changes['hero'] && this.hero) {
+    //   this.heroForm.patchValue({
+    //     ...this.hero,
+    //     items: this.hero.items || [], // TEMPORARY: Ensure items is an empty array if not provided
+    //   });
+    // }
+    if (changes['hero'] && this.hero) {
+      this.heroForm.patchValue({
+        name: this.hero.Name,
+        firstName: this.hero.FirstName,
+        lastName: this.hero.LastName,
+        superPower: this.hero.SuperPower,
+        location: this.hero.Location,
+        items: this.hero.items || [], // Ensure to handle if images are undefined
+      });
+    }
     console.log('selecting a hero to create/edit', this.hero);
   }
 
@@ -40,9 +72,14 @@ export class EditHeroComponent {
       .subscribe((heroes: SuperHero[]) => this.heroesUpdated.emit(heroes));
   }
 
-  createHero(hero: SuperHero) {
+  createHero() {
+    const hero = this.heroForm.value as SuperHero;
     this.superHeroService
       .createHero(hero)
       .subscribe((heroes: SuperHero[]) => this.heroesUpdated.emit(heroes));
+  }
+
+  checkHero(hero: SuperHero) {
+    console.log(hero);
   }
 }
